4e7678b6bd92a939b147a271947e318f0e3bb785
K.K. Lai <kk.lai@aventres.com>
2018-01-06 17:38:54 +0800

    Adjust speed initial value and led port

--- Test/PWM/pwm_test/pwm_test.ino
+++ Test/PWM/pwm_test/pwm_test.ino
@@ -3,19 +3,21 @@ int motorSpeed = 0;
 int ledPin = 9;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  motorSpeed=160;
+   
 }
 
 void loop() {
   // full speed
-  analogWrite(motorPin, 52);
-  delay(3000);
-  analogWrite(motorPin, 39);
-  delay(3000);
-  analogWrite(motorPin, 26);
-  delay(3000);
-  analogWrite(motorPin, 13);
-  delay(3000);
-  analogWrite(motorPin, 0);
-  delay(3000);
+  analogWrite(motorPin, motorSpeed);
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(4500);
+  motorSpeed=motorSpeed+10;
+  if (motorSpeed==210) {
+    motorSpeed=160;
+  }
+  
 }
